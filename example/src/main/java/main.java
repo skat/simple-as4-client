7ae75fd6527b79d782ba77b776079e8c53507f08
nc-cbm <cbm@netcompany.com>
2021-07-27 13:43:52 +0200

    Expanded gitignore.

--- example/src/main/java/main.java
+++ example/src/main/java/main.java
@@ -8,11 +8,15 @@ import java.net.URL;
 
 public class main {
     public static void main(String[] args) throws MalformedURLException {
-        As4ClientInterface client = new As4ClientBuilderInstance()
-                .builder()
-                .setEndpoint(new URL("bla"))
-                .setCrypto("bla")
-                .setUserNameTokenDetails("bla","bla")
-                .build();
+        As4ClientInterface client = new As4ClientBuilderInstance().builder().
+            setEndpoint(new URL("https://tst02b2bidmz1.dmz23.local:6384/exchange/CVR_13116482_UID_50151991")).
+            setCrypto("as4crypto-holodeck.properties").
+            setUserNameTokenDetails("CVR_13116482_UID_50151991", "BKOMamgh13").
+            build();
+
+        //Message messageObj = client.encrypt("xmlfilepath");
+        //Reply reply = client.send(messageObj);
+
+
     }
 }
