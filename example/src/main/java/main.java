b055cfc7d1c746d53ed04d04cd0b411f56356467
nc-cbm <cbm@netcompany.com>
2021-07-27 12:37:41 +0200

    Fixed some dependencies that may be needed.

--- example/src/main/java/main.java
+++ example/src/main/java/main.java
@@ -1,7 +1,18 @@
 import dk.toldst.eutk.as4client.As4Client;
+import dk.toldst.eutk.as4client.As4ClientInterface;
+import dk.toldst.eutk.as4client.builder.As4ClientBuilder;
+import dk.toldst.eutk.as4client.builder.support.As4ClientBuilderInstance;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws MalformedURLException {
+        As4ClientInterface client = new As4ClientBuilderInstance()
+                .builder()
+                .setEndpoint(new URL("bla"))
+                .setCrypto("bla")
+                .setUserNameTokenDetails("bla","bla")
+                .build();
     }
 }
